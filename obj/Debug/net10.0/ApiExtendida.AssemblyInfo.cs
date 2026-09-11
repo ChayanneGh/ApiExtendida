2eeadb9cb3fbd5aa76cc82592374d9284aab12ed
Chayanne <chayanmejia12@gmail.com>
2026-09-11 17:37:20 -0400

    css: reparo la vista de index en moviles

--- obj/Debug/net10.0/ApiExtendida.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiExtendida.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiExtendida")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28bf6f602141ba402e9a400225d12ff073023587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4f9dd6ca1b1591ee07c6e50f39e261547fec75")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiExtendida")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiExtendida")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
