899bbebe7afec42d3f84377ab1497f7951827895
Chayanne <chayanmejia12@gmail.com>
2026-09-19 09:05:22 -0400

    Fix: remover archivos basura de compilacion

--- Program.cs
+++ Program.cs
@@ -19,10 +19,6 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// 1. ⚠️ AGREGA ESTAS DOS LÍNEAS JUNTAS EN ESTE ORDEN:
-app.UseDefaultFiles(); // Obliga a buscar automáticamente tu archivo 'index.html' en la raíz
-app.UseStaticFiles();  // Habilita la lectura de carpetas físicas como 'css/' e 'index.html'
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
